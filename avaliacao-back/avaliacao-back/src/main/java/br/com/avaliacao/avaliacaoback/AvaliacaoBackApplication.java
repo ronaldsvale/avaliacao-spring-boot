d69677d448fc7bcf52ffea0a81caadeb262fd4d0
ronaldsvale <ronald.svale@hotmail.com>
2023-10-30 10:53:18 -0300

    enviando o projeto inicial pro github

---
+++ avaliacao-back/avaliacao-back/src/main/java/br/com/avaliacao/avaliacaoback/AvaliacaoBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.avaliacao.avaliacaoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoBackApplication.class, args);
+	}
+
+}
---
+++ avaliacao-back/avaliacao-back/src/test/java/br/com/avaliacao/avaliacaoback/AvaliacaoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avaliacao.avaliacaoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
